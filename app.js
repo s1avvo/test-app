615a7d5166e3cfb30f62b067bc8f4ec07aa5eb63
s1avo <s.nadolny@hotmail.com>
2022-09-21 21:48:56 +0200

    commit 2: test

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require("path");
 
-// const indexRouter = require('./routes/index');
+const indexRouter = require('./index');
 
 
 const app = express()
@@ -10,7 +10,7 @@ app.use(express.json());
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-// app.use('/', indexRouter);
+app.use('/', indexRouter);
 
 module.exports = app;
 
