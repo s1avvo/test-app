2520ecd126c1fd7aaa2b2e26ada056bbe1554c45
s1avo <s.nadolny@hotmail.com>
2022-09-22 18:25:24 +0200

    commit 2: test2

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'ExpressTest s1avo' });
+  res.render('index', { title: 'ExpressTest s1avo 2' });
 });
 
 module.exports = router;
